8e5b38f80f16f4dc1f38dbd4e3594a9a1752762c
Arima <andre.arima@maximatech.com>
2021-06-22 19:54:59 -0300

    inicial

---
+++ src/ArimInvest.UI/Models/CalculadoraPrecoMedio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArimInvest.UI.Models
+{
+    public class CalculadoraPrecoMedio
+    {
+        public CalculadoraPrecoMedio(List<Compra> compras)
+        {
+            Quantidade = compras.Sum(x => x.Quantidade);
+            TotalCompra = compras.Sum(x => x.Quantidade * Convert.ToDecimal(x.PrecoCompra));
+            PrecoMedio = TotalCompra / Quantidade;
+        }
+
+        public decimal PrecoMedio { get; private set; }
+        public decimal TotalCompra { get; private set; }
+        public int Quantidade { get; private set; }
+    }
+}
---
+++ src/ArimInvest.UI/Models/Compra.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArimInvest.UI.Models
+{
+    public class Compra
+    {
+        public Compra()
+        {
+            DataCompra = DateTime.Now;
+            Quantidade = 1;
+        }
+        public Compra(DateTime dataCompra, int quantidade, string precoCompra, string nomeAcao)
+        {
+            DataCompra = dataCompra;
+            Quantidade = quantidade;
+            PrecoCompra = precoCompra;
+            NomeAcao = nomeAcao;
+        }
+
+        [Required]
+        public DateTime DataCompra { get; set; }
+
+        [Required(ErrorMessage ="Informe a Quantidade")]
+        [Range(0, 300, ErrorMessage = "O valor minimo é {0} e o máximo é {1}")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o Preço de Compra")]
+        [RegularExpression("^[-,0-9]+$", ErrorMessage = "Somente números")]
+        public string PrecoCompra { get; set; }
+        public string NomeAcao { get; set; }
+        public string TotalCompra()
+        {
+            return (Quantidade * Convert.ToDecimal(PrecoCompra)).ToString("C2");
+        }
+    }
+}
